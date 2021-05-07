55c3372c78be6e50601d0de7984b9cfd4c796546
Niall Murphy <c17313343@mydit.ie>
2021-05-07 13:42:06 +0100

    tested student toString()

--- test/StudentTest.java
+++ test/StudentTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 public class StudentTest {
 
@@ -21,6 +22,33 @@ public class StudentTest {
     public void nullStudentNameTest(){
         Student student = new Student();
         assertEquals(student.getName(), "");
+    }
+
+    @Test
+    public void studentToStringTest() {
+
+        ArrayList<StudentGrade> exampleGrades = new ArrayList<>();
+        StudentGrade exampleGrade1 = new StudentGrade("Account Check", 5);
+        StudentGrade exampleGrade2 = new StudentGrade("Balance Sheet", 3);
+        StudentGrade exampleGrade3 = new StudentGrade("COA", 4);
+        StudentGrade exampleGrade4 = new StudentGrade("Endowment", 5);
+
+        exampleGrades.add(exampleGrade1);
+        exampleGrades.add(exampleGrade2);
+        exampleGrades.add(exampleGrade3);
+        exampleGrades.add(exampleGrade4);
+
+        Student testStudent = new Student("Chelsea Blakemore", exampleGrades);
+
+        String actual = "Student{" +
+                "name='" + "Chelsea Blakemore" + '\'' +
+                ", grades=" + testStudent.getGrades().toString() +
+                '}';
+
+
+        assertEquals(testStudent.toString(), actual);
+
+
     }
 
 }
