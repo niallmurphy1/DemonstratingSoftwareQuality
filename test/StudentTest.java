14ab0332bafd04c88b8d1961ce294854a15e5fbf
Niall Murphy <c17313343@mydit.ie>
2021-05-07 13:31:52 +0100

    testing Student values. null on student name

--- test/StudentTest.java
+++ test/StudentTest.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+import entities.Student;
+import entities.StudentGrade;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
 
 public class StudentTest {
+
+
+    @Test
+    public void nullStudentGradeTest(){
+        Student student = new Student();
+
+        assertEquals(student.getGrades(), new ArrayList<StudentGrade>());
+    }
+
+
+    @Test
+    public void nullStudentNameTest(){
+        Student student = new Student();
+        assertEquals(student.getName(), "");
+    }
+
 }
